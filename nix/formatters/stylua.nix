793650dc1eb1f9037f42a450c341a6fe1ab6b452
Christian Gram Kalhauge <chrg@dtu.dk>
2026-01-09 04:59:43 +0100

    Add indent option to stylua

--- nix/formatters/stylua.nix
+++ nix/formatters/stylua.nix
@@ -1,3 +1,33 @@
 {
-  args = [ "-" ];
+  config,
+  options,
+  lib,
+  ...
+}:
+{
+  options = {
+    indent = lib.mkOption {
+      type = lib.types.nullOr lib.types.int;
+      default = null;
+      description = "number of spaces to indent, null is tabs";
+    };
+  };
+
+  config = {
+    args =
+      (
+        if config.indent != null then
+          [
+            "--indent-type"
+            "Spaces"
+            "--indent-width"
+            (builtins.toString config.indent)
+          ]
+        else
+          [ ]
+      )
+      ++ [
+        "-"
+      ];
+  };
 }
